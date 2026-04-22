5ec4574fa605cbd4f65537bd5c7160373ca2b689
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2026-04-22 10:37:46 +0900

    プロジェクトファイルを追加。

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,53 @@
+#include "GameScene.h"
+#include "KamataEngine.h"
+#include <Windows.h>
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	// エンジンの初期化
+	KamataEngine::Initialize(L"LE3D_18_ビョウドウ_トモヒロ_AL3");
+
+	// DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+
+	// ゲームシーンの初期化
+	gameScene->Initialize();
+
+	// メインループ
+	while (true) {
+
+		// エンジンの更新
+		if (KamataEngine::Update()) {
+
+			break;
+		}
+
+		// ゲームシーンの更新
+		gameScene->Update();
+
+		// 描画開始
+		dxCommon->PreDraw();
+
+		// ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
+	}
+
+	// ゲームシーンの解放
+	delete gameScene;
+
+	// nullptrの代入
+	gameScene = nullptr;
+
+	// エンジンの終了宣言
+	KamataEngine::Finalize();
+
+	return 0;
+}
